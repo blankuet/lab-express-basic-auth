eea08abf57da361f6a7339ad7bd0ca67b6bb39e6
blankuet <blancafeijoocruz@gmail.com>
2024-06-04 17:24:46 +0200

    iteration 1 done

--- models/User.model.js
+++ models/User.model.js
@@ -4,7 +4,7 @@ const { Schema, model } = require("mongoose");
 const userSchema = new Schema({
   username: {
     type: String,
-    required: false,
+    required: true,
     unique: true,
     trim: true,
   },
--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,19 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-router.post("/signup", (req, res) => {
-  res.json(req.body)
-})
+router.post('/signup', async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'Todos los campos son obligatorios.' });
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await User.create({ username, email, password: hashedPassword });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al crear el usuario.' });
+  }
+});
+
 module.exports = router;
